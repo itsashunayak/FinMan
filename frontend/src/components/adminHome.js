fe83ab77d28338b673ea53abcf895f449f0b2881
Ashutosh Nayak <96097511+itsashunayak@users.noreply.github.com>
2023-07-27 12:02:14 +0000

    commit from git

--- frontend/src/components/adminHome.js
+++ frontend/src/components/adminHome.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState } from "react";
-import { faAdd, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ReactPaginate from 'react-paginate';
 import { useRef } from "react";
